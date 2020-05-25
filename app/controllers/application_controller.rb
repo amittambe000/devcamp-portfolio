63c0dfffb17a5d92147877d451d4745e4d3886cd
Amit Tambe <amittambe0000@gmail.com>
2020-05-25 10:17:04 +0530

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
 
 
+  before_action :set_title
+
+  def set_title
+    @page_title="Devcamp Portfolio | My Portfolio Website"
+  end
+
 end
