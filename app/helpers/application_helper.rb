63460103fdf363dadf19bfd230885e64ff91e99a
Amit Tambe <amittambe0000@gmail.com>
2020-06-13 13:13:13 +0530

    Implemented refactor for nav and status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_helper style=''
+  def login_helper style=""
      if !current_user.is_a?(GuestUser)
-      link_to "Logout", destroy_user_session_path, method: :delete,class: style
+      link_to "Logout", destroy_user_session_path, method: :delete,class: "#{style} #{active? destroy_user_session_path} "
     else
-      (link_to "Sign Up", new_user_registration_path,class: style)+
+      (link_to "Sign Up", new_user_registration_path,class: "#{style} #{active? new_user_registration_path} ")+
       " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      (link_to "Login", new_user_session_path, class: "#{style} #{active? new_user_session_path} ")
     end
   end
 
@@ -20,4 +20,58 @@ module ApplicationHelper
   def copyright_generator
     ViewToolNonsenseTester::Renderer.copyright 'Amit Tambe|','All rights reserved'
   end
-end
+
+  def nav_items
+    [
+        {
+            url:root_path,
+            title:'Home'
+        },
+        {
+            url:about_me_path,
+            title:'About Me'
+        },
+        {
+            url:contact_path,
+            title:'Contact'
+        },
+        {
+            url:blogs_path,
+            title:'Blog'
+        },
+        {
+            url:portfolios_path,
+            title:'Portfolio'
+        }
+    ]
+  end
+
+  def nav_helper style,tag_type
+    nav_links=''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class= '#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def nav_helper1 style,tag_type
+nav_links= <<NAV
+<#{tag_type}><a href="#{root_path}" class= "#{style} #{active? root_path}"> Home</a> </#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}" class= "#{style} #{active? about_me_path}"> About Me</a> </#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class= "#{style} #{active? contact_path}"> Contact</a> </#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class= "#{style} #{active? blogs_path}"> Blogs</a> </#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}"> Portfolio</a> </#{tag_type}>
+
+NAV
+    nav_links.html_safe
+  end
+
+
+
+  def active? path
+
+    "active" if current_page? path
+  end
+
+
+  end
