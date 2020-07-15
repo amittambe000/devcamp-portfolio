57961e4ef9e7b698440df40f216d38eef49da8f4
Amit Tambe <amittambe0000@gmail.com>
2020-07-15 17:23:32 +0530

    Refactored alerts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require ("jquery");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,5 +20,6 @@ require("channels")
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
-
-
+//= require gritter
+//require cocoon
+import 'cocoon-js'
